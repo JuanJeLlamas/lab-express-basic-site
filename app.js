ed1af0bdaebd1d487acdd837e7a8970bab0a33e4
jdluis <jdluisdev@gmail.com>
2023-02-07 16:37:25 +0000

    "refact route"

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-const express = require("express")
-const app = express ()
-const port = 3000; 
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
 
-app.get('/', (req, res) => {
-    res.sendFile
-    (__dirname + "/views/home.html")
-  })
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
+});
 
-  app.get('/about', (req, res) => {
-    res.sendFile
-    (__dirname + "/views/about.html")
-  })
-
-
-  app.listen(port, ()=> {
-console.log("batman returns puerto: " + port )
-
-  })
+app.listen(port, () => {
+  console.log("batman returns puerto: " + port);
+});
