c76165a4bfb748255c0eb57bc0b3a63093357c78
JuanJe <juanje.llamas@gmail.com>
2023-02-07 17:30:15 +0100

    transaccion 2

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express ()
+const port = 3000; 
+
+
+app.get('/', (req, res) => {
+    res.sendFile
+    (__dirname + "/views/home.html")
+  })
+
+  app.listen(port, ()=> {
+console.log("batman returns puerto: " + port )
+
+  })
